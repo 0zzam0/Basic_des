97ba017763b89a0fb630c89a2c7b5b2dc3ecdf2e
0zzam0 <78575152+0zzam0@users.noreply.github.com>
2022-02-04 19:53:45 +0900

    20220204 IP구현/hex

---
+++ Project1_des/head.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdio.h>
+
+
+char Round();
+char Xor();
+char F();
+char Ip();
+char Ip_();
+char E();
+char S();
+char P();
+int charToBit();
+char BitToChar();
---
+++ head.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+
+char Round();
+char Xor();
+char F();
+char Ip();
+char Ip_();
+char E();
+char S();
+char P();
+int charToBit();
+char BitToChar();
